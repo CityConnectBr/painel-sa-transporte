bc95ddd358630eead81318e65dc29a53c5c050f3
Elon Gomes Vieira <elongvieira@yahoo.com.br>
2021-07-28 20:26:59 -0300

    Troca do projeto do Flutter para Angular

--- lib/controllers/common_controller.dart
+++
@@ -1,7 +0,0 @@
-import 'package:get/get.dart';
-import 'package:web_sa_transportes/models/usuario_model.dart';
-
-class CommonController extends GetxController {
-  //variaveis
-  var usuarioLogged = Usuario().obs;
-}
--- lib/pages/login/login_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:web_sa_transportes/pages/login/login_controller.dart';
-
-class LoginBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => LoginController());
-  }
-}
