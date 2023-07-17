eec0eb75bdb2f313a84267aa5ac7a6847cbd2a22
Elon Gomes Vieira <elongvieira@yahoo.com.br>
2023-07-17 20:09:54 -0300

    Adição de campos data emissao, nome e descrição em curso de permissionario, condutor e monitor

--- src/app/models/curso-do-condutor.ts
+++ src/app/models/curso-do-condutor.ts
@@ -1,7 +1,12 @@
+import { TipoDeCurso } from "./tipo-de-curso"
+
 export interface CursoDoCondutor {
   id: string
   condutor_id: string
   tipo_do_curso_id: string
+  tipo_de_curso: TipoDeCurso
   data_emissao: Date
+  data_validade: Date
+  nome: string
   descricao: string
 }
--- src/app/models/curso-do-permissionario.ts
+++ src/app/models/curso-do-permissionario.ts
@@ -1,7 +1,12 @@
+import { TipoDeCurso } from "./tipo-de-curso"
+
 export interface CursoDoPermissionario {
   id: string
   permissionario_id: string
   tipo_do_curso_id: string
-  desc_descricao_do_tipo: string
+  tipo_de_curso: TipoDeCurso
   data_emissao: Date
+  data_validade: Date
+  nome: string
+  descricao: string
 }
