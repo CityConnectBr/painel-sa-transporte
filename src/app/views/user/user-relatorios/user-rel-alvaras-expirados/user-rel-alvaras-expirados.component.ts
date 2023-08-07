7432199fc57adbe59ec63560e0bb99432305f291
Elon Gomes Vieira <elongvieira@yahoo.com.br>
2023-08-07 18:39:13 -0300

    Adição de filtro em relatórios de alvara

--- src/app/views/user/user-relatorios/user-rel-alvaras-expirados/user-rel-alvaras-expirados.component.ts
+++ src/app/views/user/user-relatorios/user-rel-alvaras-expirados/user-rel-alvaras-expirados.component.ts
@@ -1,31 +1,56 @@
+import { ModalidadeService } from './../../../../services/modalidade.service';
 import { Component, OnInit } from '@angular/core';
+import { Modalidade } from 'src/app/models/modalidade';
 import { RelatorioService } from 'src/app/services/relatorio.service';
 
 @Component({
   selector: 'app-user-rel-alvaras-expirados',
   templateUrl: './user-rel-alvaras-expirados.component.html',
-  styleUrls: ['./user-rel-alvaras-expirados.component.css']
+  styleUrls: ['./user-rel-alvaras-expirados.component.css'],
 })
 export class UserRelAlvarasExpiradosComponent implements OnInit {
-
   loading: boolean = false;
 
   alvarasExpirados: any[] = [];
+  alvarasExpiradosToShow: any[] = [];
+
+  modalidades: Modalidade[];
+
+  modalidade: string = '';
 
   constructor(
     private relatorioService: RelatorioService,
-  ) { }
+    private modalidadeService: ModalidadeService
+  ) {}
 
   ngOnInit() {
     this.gerarRelatorio();
+
+    this.loadModalidades();
   }
 
   gerarRelatorio() {
-    this.relatorioService.getAlvarasExpirados().subscribe(
-      (data) => {
-        this.alvarasExpirados = data;
-      }
-    )
+    this.relatorioService.getAlvarasExpirados().subscribe((data) => {
+      this.alvarasExpirados = data;
+      this.alvarasExpiradosToShow = data;
+    });
   }
 
+  loadModalidades() {
+    this.modalidadeService.index().subscribe((data) => {
+      this.modalidades = data;
+    });
+  }
+
+  filter() {
+    this.alvarasExpiradosToShow = this.alvarasExpirados.filter((alvara) => {
+      if (this.modalidade) {
+        if (alvara.permissionario.modalidade_id == this.modalidade) {
+          return false;
+        }
+      }
+
+      return true;
+    });
+  }
 }
